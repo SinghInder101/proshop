772e4df5f45d0558df3f35bd206e91ef5b8c5f39
SinghInder101 <inderbirsingh03@gmail.com>
2024-08-28 16:48:06 -0400

    Data Models

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 dotenv.config()
 const port = process.env.PORT || 5001;
+connectDB();
 
 const app = express();
 
