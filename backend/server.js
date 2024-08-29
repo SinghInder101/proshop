1266543c29569776c8abfc1af22b51c81af891ef
SinghInder101 <inderbirsingh03@gmail.com>
2024-08-29 13:18:07 -0400

    Controllers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import products from './data/products.js';
+
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js'
 dotenv.config()
 const port = process.env.PORT || 5001;
 connectDB();
@@ -12,11 +14,10 @@ const app = express();
 app.get('/',(req,res) => {
     res.send('API is running');
 })
-app.get('/api/products',(req,res)=> {
-    res.json(products);
-})
-app.get('/api/products/:id',(req,res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product);
-})
+
+app.use('/api/products',productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(port,() => console.log('Server running'))
