5dd4bb31ebd4e9f738bc7aa444bf0ae741436dda
SinghInder101 <inderbirsingh03@gmail.com>
2024-08-30 17:38:18 -0400

    cart screen implemented

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -13,13 +13,10 @@ const cartSlice = createSlice({
 
             const existItem = state.cartItems.find((x) => x._id === item._id)
             if(existItem){
-                state.cartItems = state.cartItems.map((cartItem) => {
 
-                    if(cartItem._id === existItem._id){
-                        return item;
-                    }
-
-                })
+                state.cartItems = state.cartItems.map((cartItem) => 
+    cartItem._id === existItem._id ? item : cartItem
+);
             }
             else {
                 state.cartItems = [...state.cartItems,item]
@@ -30,11 +27,20 @@ const cartSlice = createSlice({
             //Calculate price
             
 
+        },
+        removeFromCart: (state,action) =>{
+            const item = action.payload;
+            console.log(item);
+
+           state.cartItems.splice(state.cartItems.indexOf(item),1);
+           return updateCart(state);
+
+
         }
 
     }
 })
 
-export const {addToCart} = cartSlice.actions;
+export const {addToCart,removeFromCart} = cartSlice.actions;
 
 export default cartSlice.reducer;
